935c3f3cc312dcecb17337fa4acc9a8582e0989f
Aadesh singh <aadeshsingh146@gmail.com>
2024-10-29 23:52:20 +0530

    setting page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,3 +1,6 @@
+import { IoSettings } from "react-icons/io5";
+import { NavLink, Outlet } from "react-router-dom";
+
 const Settings = () => {
   return (
     <section id={"settings"} className="bg-neutral-950 p-6">
@@ -6,8 +9,50 @@ const Settings = () => {
         <hr />
       </div>
       <div className="flex justify-between items-start bg-neutral-950 text-white min-h-[75vh] p-5">
-        <div className="basis-1/4 h-[70vh] overflow-auto">Sidebar</div>
-        <div className="basis-3/4 h-[70vh] overflow-auto">Main Setting</div>
+        <div className="basis-1/4 h-[70vh] overflow-auto bg-galvin-grey m-2 rounded-md">
+          <div className="side-container container max-h-[74%] overflow-y-auto scrollbar-thinscrollbar-thumb-gray-500 scrollbar-track-gray-900 hover:scrollbar-thumb-gray-400">
+            <NavLink
+              to={`/settings/`}
+              className="flex justify-start items-center p-2 hover:bg-galvin-green"
+            >
+              <div className="m-2 text-xl">
+                <IoSettings />
+              </div>
+              <div className="m-2 text-xl">Settings</div>
+            </NavLink>
+            <NavLink
+              to={`/settings/create-playlist`}
+              className="flex justify-start items-center p-2 hover:bg-galvin-green"
+            >
+              <div className="m-2 text-xl">
+                <IoSettings />
+              </div>
+              <div className="m-2 text-xl">Create Playlist</div>
+            </NavLink>
+            <NavLink
+              to={`/settings/create-album`}
+              className="flex justify-start items-center p-2 hover:bg-galvin-green"
+            >
+              <div className="m-2 text-xl">
+                <IoSettings />
+              </div>
+              <div className="m-2 text-xl">Create Album</div>
+            </NavLink>
+            <NavLink
+              to={`/settings/upload-song`}
+              className="flex justify-start items-center p-2 hover:bg-galvin-green"
+            >
+              <div className="m-2 text-xl">
+                <IoSettings />
+              </div>
+              <div className="m-2 text-xl">Upload Song</div>
+            </NavLink>
+          </div>
+        </div>
+
+        <div className="basis-3/4 h-[70vh] overflow-auto bg-galvin-bg m-2 rounded-md">
+          <Outlet />
+        </div>
       </div>
     </section>
   );
